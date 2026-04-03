97005e39c6c455aadec4a3ea1796f77a74bd23e0
talha-ansarii <talhaansari1606@gmail.com>
2026-04-03 22:32:18 +0530

    making vercel comapatible

--- backend/database/session.py
+++ backend/database/session.py
@@ -4,8 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from core.config import settings
 
+connect_args = {"check_same_thread": False} if settings.DATABASE_URL.startswith("sqlite") else {}
 engine = create_engine(
-    settings.DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
